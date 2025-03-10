620a819abdd34be3f2f69330471ee911353c8754
razhzq <razhaziq@gmail.com>
2025-03-10 12:53:23 +0800

    add 0x

--- src/utils/contract.utils.ts
+++ src/utils/contract.utils.ts
@@ -16,12 +16,10 @@ export async function mintNFT(action: ClaimAction, signal: `0x${string}`, tokenI
         // if (!env.PRIVATE_KEY) {
         //     throw new Error('PRIVATE_KEY is not defined in environment variables');
         // }
-        //console.log('env: ', env);
-        const privateKey = env.PRIVATE_KEY;
-            // ? env.PRIVATE_KEY 
-            // : `0x${env.PRIVATE_KEY}`;
+        const privKey = env.PRIVATE_KEY;
+        const privateKey = `0x${privKey}`;
             
-        const account = privateKeyToAccount(privateKey as string);
+        const account = privateKeyToAccount(privateKey as Hex.Hex);
         const client = createWalletClient({
             account,
             chain: worldchain,
