54fab1cbf08de660d1d3c2a623e61b23b5470713
DanielHighETH <danielhigheth@gmail.com>
2025-03-31 17:19:33 +0800

    dev testing

--- src/config/constant.ts
+++ src/config/constant.ts
@@ -15,7 +15,8 @@ export const WORLDSCAN_PATH = {
 	notification: "https://developer.worldcoin.org/api/v2/minikit/send-notification"
 }
 
-export const WORLDCHAIN_RPC_URL = "https://worldchain-sepolia.gateway.tenderly.co"//"https://chaotic-fabled-water.worldchain-sepolia.quiknode.pro/3f033f9e1b6829b9b352bed07914e122c10ec1d4/"//"https://icy-greatest-dream.worldchain-mainnet.quiknode.pro/f465b27e9a8dbd90d16bfb3bca7b2193bc1dcfa8/"
+//https://worldchain-sepolia.gateway.tenderly.co
+export const WORLDCHAIN_RPC_URL = "https://icy-greatest-dream.worldchain-mainnet.quiknode.pro/f465b27e9a8dbd90d16bfb3bca7b2193bc1dcfa8/"//"https://chaotic-fabled-water.worldchain-sepolia.quiknode.pro/3f033f9e1b6829b9b352bed07914e122c10ec1d4/"//"https://icy-greatest-dream.worldchain-mainnet.quiknode.pro/f465b27e9a8dbd90d16bfb3bca7b2193bc1dcfa8/"
 export const WORLDCHAIN_RPC_URL_V2 = "https://chaotic-fabled-water.worldchain-sepolia.quiknode.pro/3f033f9e1b6829b9b352bed07914e122c10ec1d4/"
 export const WORLDCHAIN_RPC_URL_V3 = "https://worldchain-sepolia.g.alchemy.com/v2/Cj_65mRSyh2uxEc9RCI6JhMQSOVwz56m"
 export const WORLDCHAIN_ALCHEMY_RPC_URL = "https://worldchain-mainnet.g.alchemy.com/v2/Cj_65mRSyh2uxEc9RCI6JhMQSOVwz56m"
